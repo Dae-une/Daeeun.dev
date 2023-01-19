109cd8a72bfd2717a556705aedc374e7775188ca
Dae-une <ko301201@naver.com>
2023-01-20 02:33:05 +0900

    post: about useCallback

--- .contentlayer/generated/Post/_index.mjs
+++ .contentlayer/generated/Post/_index.mjs
@@ -2,5 +2,6 @@
 
 import executionContextMdx from './Execution Context.mdx.json' assert { type: 'json' }
 import staleWhileRevalidateMdx from './Stale-While-revalidate.mdx.json' assert { type: 'json' }
+import aboutUseCallbackMdx from './about useCallback.mdx.json' assert { type: 'json' }
 
-export const allPosts = [executionContextMdx, staleWhileRevalidateMdx]
+export const allPosts = [executionContextMdx, staleWhileRevalidateMdx, aboutUseCallbackMdx]
