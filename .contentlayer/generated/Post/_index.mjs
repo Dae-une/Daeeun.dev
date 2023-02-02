41228a34cb1ede5af14f0c39e3faa29d6452b84f
Dae-une <ko301201@naver.com>
2023-02-02 16:44:31 +0900

    post: cure_for_useState_hell

--- .contentlayer/generated/Post/_index.mjs
+++ .contentlayer/generated/Post/_index.mjs
@@ -3,8 +3,9 @@
 import closuresMdx from './Closures.mdx.json' assert { type: 'json' }
 import execution_contextMdx from './Execution_Context.mdx.json' assert { type: 'json' }
 import staleWhileRevalidateMdx from './Stale-While-revalidate.mdx.json' assert { type: 'json' }
-import about_useCallbackMdx from './about_useCallback.mdx.json' assert { type: 'json' }
 import why_is_setState_asynchronousMdx from './Why_is_setState_asynchronous.mdx.json' assert { type: 'json' }
 import a11yMdx from './a11y.mdx.json' assert { type: 'json' }
+import about_useCallbackMdx from './about_useCallback.mdx.json' assert { type: 'json' }
+import cure_for_useState_hellMdx from './Cure_for_useState_Hell.mdx.json' assert { type: 'json' }
 
-export const allPosts = [closuresMdx, execution_contextMdx, staleWhileRevalidateMdx, about_useCallbackMdx, why_is_setState_asynchronousMdx, a11yMdx]
+export const allPosts = [closuresMdx, execution_contextMdx, staleWhileRevalidateMdx, why_is_setState_asynchronousMdx, a11yMdx, about_useCallbackMdx, cure_for_useState_hellMdx]
