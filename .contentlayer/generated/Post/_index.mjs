ad069d3e1ce3a1e2af7b73772923d28f5440c8e9
Dae-une <ko301201@naver.com>
2023-02-17 14:41:28 +0900

    post : Image_Optimization

--- .contentlayer/generated/Post/_index.mjs
+++ .contentlayer/generated/Post/_index.mjs
@@ -3,10 +3,11 @@
 import closuresMdx from './Closures.mdx.json' assert { type: 'json' }
 import cure_for_useState_hellMdx from './Cure_for_useState_Hell.mdx.json' assert { type: 'json' }
 import execution_contextMdx from './Execution_Context.mdx.json' assert { type: 'json' }
+import image_optimizationMdx from './Image_Optimization.mdx.json' assert { type: 'json' }
 import staleWhileRevalidateMdx from './Stale-While-revalidate.mdx.json' assert { type: 'json' }
 import why_is_setState_asynchronousMdx from './Why_is_setState_asynchronous.mdx.json' assert { type: 'json' }
 import a11yMdx from './a11y.mdx.json' assert { type: 'json' }
 import about_useCallbackMdx from './about_useCallback.mdx.json' assert { type: 'json' }
 import concureent_ui_patternMdx from './concureent_UI_Pattern.mdx.json' assert { type: 'json' }
 
-export const allPosts = [closuresMdx, cure_for_useState_hellMdx, execution_contextMdx, staleWhileRevalidateMdx, why_is_setState_asynchronousMdx, a11yMdx, about_useCallbackMdx, concureent_ui_patternMdx]
+export const allPosts = [closuresMdx, cure_for_useState_hellMdx, execution_contextMdx, image_optimizationMdx, staleWhileRevalidateMdx, why_is_setState_asynchronousMdx, a11yMdx, about_useCallbackMdx, concureent_ui_patternMdx]
